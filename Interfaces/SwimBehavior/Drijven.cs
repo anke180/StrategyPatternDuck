96b5d7753f3ef61ead749df4287ab240f8054353
anke180 <263576653+adommelenBM@users.noreply.github.com>
2026-09-03 14:56:38 +0200

    SwimBehavior created new classes

---
+++ Interfaces/SwimBehavior/Drijven.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.Interfaces.SwimBehavior
+{
+    internal class Drijven : SwimBehaviour
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Drijft op het water");
+        }
+    }
+}
---
+++ Interfaces/SwimBehavior/Zinken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.Interfaces.SwimBehavior
+{
+    internal class Zinken
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Zinkt in het water");
+        }
+    }
+}
